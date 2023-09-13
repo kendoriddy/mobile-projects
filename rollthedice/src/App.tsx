231c4e98c5d2bfa1b944ff3ff9a82ccabada6939
Kehinde Onifade <onifkay@gmail.com>
2023-09-13 20:22:17 +0100

    firrst commit

---
+++ rollthedice/src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import DiceOne from '../assets/One.png';
+import DiceTwo from '../assets/Two.png';
+import DiceThree from '../assets/Three.png';
+import DiceFour from '../assets/Four.png';
+import DiceFive from '../assets/Five.png';
+import DiceSix from '../assets/Six.png';
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return <Image source={imageUrl} style={styles.diceImage} />;
+};
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Dice imageUrl={DiceOne} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
+
+export default App;
